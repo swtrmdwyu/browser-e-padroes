3caba0bdcda29e602bc6c979e36c503e103ec0cb
swtrmdwyu <swtrmdwyu@gmail.com>
2023-09-26 13:58:36 -0300

    Encapsulamento

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,12 +1,24 @@
 class Negociacao {
     constructor(data, quantidade, valor) {
-        this.data = data,
-        this.quantidade = quantidade,
-        this.valor = valor
+        this._data = data,
+        this._quantidade = quantidade,
+        this._valor = valor
         //this.volume = this.valor * this.quantidade
     }
 
     bringVolume() {
-        return this.quantidade * this.valor;
+        return this._quantidade * this.valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+    
+    getQuantidade() {
+        return this._quantidade;
+    }
+    
+    getValor() {
+        return this._valor;
     }
 }
